ad4d4483269643e1feb7fa80731543da4ba9a6f5
Benson Peng <penglingzhe@gmail.com>
2011-08-01 21:52:05 +0800

    add GroupBuyUserService

---
+++ GroupBuyCommon/Service/GroupBuyUserService.m
@@ -0,0 +1,14 @@
+//
+//  GroupBuyUserService.m
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-1.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GroupBuyUserService.h"
+
+
+@implementation GroupBuyUserService
+
+@end
