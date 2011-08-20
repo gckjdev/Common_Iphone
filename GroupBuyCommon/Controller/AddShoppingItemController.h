07e0e2724af1a6d958180051cbe1ac90d639acb7
Bill Chen <gspace4ebook@gmail.com>
2011-08-20 14:23:52 +0800

    add new files for shoping list feature.

---
+++ GroupBuyCommon/Controller/AddShoppingItemController.h
@@ -0,0 +1,16 @@
+//
+//  AddShoppingItemController.h
+//  groupbuy
+//
+//  Created by LouisLee on 11-8-20.
+//  Copyright 2011 ET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AddShoppingItemController : UIViewController {
+
+}
+
+@end
---
+++ GroupBuyCommon/Controller/ShoppingListController.h
@@ -0,0 +1,16 @@
+//
+//  ShoppingListController.h
+//  groupbuy
+//
+//  Created by LouisLee on 11-8-20.
+//  Copyright 2011 ET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ShoppingListController : UIViewController {
+
+}
+
+@end
