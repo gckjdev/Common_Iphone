2fd6ed12258e76a2f9462ac822c40b8b7e800494
Bill Chen <gspace4ebook@gmail.com>
2011-08-20 15:18:17 +0800

    add new cell class for shopping list feature.

---
+++ GroupBuyCommon/Controller/ShoppingKeywordCell.h
@@ -0,0 +1,16 @@
+//
+//  ShoppingKeywordCell.h
+//  groupbuy
+//
+//  Created by LouisLee on 11-8-20.
+//  Copyright 2011 ET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PPTableViewCell.h"
+
+@interface ShoppingKeywordCell : PPTableViewCell {
+
+}
+
+@end
