67eb7c4645615af3eb6fd06429f7ec6704fa7909
Benson Peng <penglingzhe@gmail.com>
2011-08-25 22:55:09 +0800

    add user shop item service

---
+++ GroupBuyCommon/Service/UserShopItemService.h
@@ -0,0 +1,16 @@
+//
+//  UserShopItemService.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-25.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UserShopItemService : NSObject {
+    
+}
+
+@end
---
+++ GroupBuyCommon/Service/UserShopItemService.m
@@ -0,0 +1,14 @@
+//
+//  UserShopItemService.m
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-25.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserShopItemService.h"
+
+
+@implementation UserShopItemService
+
+@end
