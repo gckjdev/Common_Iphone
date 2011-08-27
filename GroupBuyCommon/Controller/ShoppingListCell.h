e5ecb09796be54a6da90127ec67c2747458daab2
Benson Peng <penglingzhe@gmail.com>
2011-08-27 17:13:50 +0800

    add shoppinglist cell

---
+++ GroupBuyCommon/Controller/ShoppingListCell.h
@@ -0,0 +1,16 @@
+//
+//  ShoppingListCell.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-27.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ShoppingListCell : UIViewController {
+    
+}
+
+@end
