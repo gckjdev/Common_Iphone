f36f6539ff414bbf2abf4af6203e1b5ee2964ebe
Benson Peng <penglingzhe@gmail.com>
2011-08-28 12:09:45 +0800

    add category manager

---
+++ GroupBuyCommon/Model/CategoryManager.h
@@ -0,0 +1,16 @@
+//
+//  CategoryManager.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-28.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CategoryManager : NSObject {
+    
+}
+
+@end
---
+++ GroupBuyCommon/Model/CategoryManager.m
@@ -0,0 +1,14 @@
+//
+//  CategoryManager.m
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-28.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "CategoryManager.h"
+
+
+@implementation CategoryManager
+
+@end
