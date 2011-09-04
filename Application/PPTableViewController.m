af5a49286fa720bd43d84647495dc3858efd47de
Benson Peng <penglingzhe@gmail.com>
2011-09-04 16:16:50 +0800

    remove unneeded log

--- Application/PPTableViewController.m
+++ Application/PPTableViewController.m
@@ -367,9 +367,6 @@
         return;
     
 	if (scrollView.isDragging) {        
-#ifdef DEBUG        
-        NSLog(@"point=%@", NSStringFromCGPoint(scrollView.contentOffset));
-#endif        
 		if (!_reloading && refreshHeaderView.state == EGOOPullRefreshPulling && scrollView.contentOffset.y > -65.0f && scrollView.contentOffset.y < 0.0f) {
 			[refreshHeaderView setState:EGOOPullRefreshNormal];
 		} else if (!_reloading && refreshHeaderView.state == EGOOPullRefreshNormal && scrollView.contentOffset.y < -65.0f) {
