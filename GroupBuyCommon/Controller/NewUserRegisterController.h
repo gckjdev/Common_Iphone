046981ba2e323fb6e64bfa36641351c59e5bf88e
Benson Peng <penglingzhe@gmail.com>
2011-09-14 16:11:39 +0800

    add register controller

---
+++ GroupBuyCommon/Controller/NewUserRegisterController.h
@@ -0,0 +1,16 @@
+//
+//  NewUserRegisterController.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-14.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NewUserRegisterController : UIViewController {
+    
+}
+
+@end
