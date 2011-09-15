122fb09d07a40fba052b9402dc7e8e4a8677c259
Benson Peng <penglingzhe@gmail.com>
2011-09-15 20:18:27 +0800

    add files

---
+++ SNS/Common/SNSWebViewController.h
@@ -0,0 +1,16 @@
+//
+//  SNSWebViewController.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-15.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SNSWebViewController : UIViewController {
+    
+}
+
+@end
