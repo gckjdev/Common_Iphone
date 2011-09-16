83126fa2eb82efa3247054f6a3502c19a59f3641
Benson Peng <penglingzhe@gmail.com>
2011-09-16 14:01:04 +0800

    add GroupBuySNSService

---
+++ GroupBuyCommon/Service/GroupBuySNSService.h
@@ -0,0 +1,16 @@
+//
+//  GroupBuySNSService.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-16.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface GroupBuySNSService : NSObject {
+    
+}
+
+@end
---
+++ GroupBuyCommon/Service/GroupBuySNSService.m
@@ -0,0 +1,14 @@
+//
+//  GroupBuySNSService.m
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-16.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GroupBuySNSService.h"
+
+
+@implementation GroupBuySNSService
+
+@end
