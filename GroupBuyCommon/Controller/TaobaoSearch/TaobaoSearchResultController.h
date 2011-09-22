cccca025f611a605fcd820d2373c8bfae7ac9c26
Benson Peng <penglingzhe@gmail.com>
2011-09-22 22:04:50 +0800

    add taobao search support

---
+++ GroupBuyCommon/Controller/TaobaoSearch/TaobaoSearchResultController.h
@@ -0,0 +1,16 @@
+//
+//  TaobaoSearchResultController.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-22.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TaobaoSearchResultController : UIViewController {
+    
+}
+
+@end
