2b940325c60e97edf4762196e7bc798f51ba0dd9
Benson Peng <penglingzhe@gmail.com>
2011-09-23 17:09:36 +0800

    update model add taobao search result

---
+++ GroupBuyCommon/Controller/TaobaoSearch/TaobaoSearchResultCell.h
@@ -0,0 +1,16 @@
+//
+//  TaobaoSearchResultCell.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-9-23.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface TaobaoSearchResultCell : UIViewController {
+    
+}
+
+@end
