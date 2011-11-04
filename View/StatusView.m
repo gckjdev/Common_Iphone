2287322d8829a9031b43d286c5a83b140a9d433b
kira <ouyongyong@163.com>
2011-11-04 15:17:39 +0800

    recover status view

--- View/StatusView.m
+++ View/StatusView.m
@@ -11,7 +11,6 @@
 #define STATUS_VIEW_TEXT_TAG                20
 #define DEFAULT_STATUS_DISPLAY_DURATION     10
 #define STATUS_VIEW_ANIMATION_DURATION      0.5
-#define STATUS_WINDOW_LEVEL                 2010
 
 StatusView *globalStatusView;
 
@@ -66,7 +65,7 @@ StatusView *globalStatusView;
     
     [statusWindow setFrame:frame];
     [statusWindow setBackgroundColor:[UIColor blackColor]];
-    [statusWindow setWindowLevel:STATUS_WINDOW_LEVEL];
+    [statusWindow setWindowLevel:UIWindowLevelStatusBar];
     [statusWindow makeKeyAndVisible];
     
     [UIView beginAnimations:@"" context:nil];
