1dac0371e532f0919bcaafd48d6a8a5dc33e79f0
Benson Peng <penglingzhe@gmail.com>
2011-11-11 13:53:26 +0800

    add item action controller

---
+++ DownloadCommon/Controller/ItemActionController.h
@@ -0,0 +1,13 @@
+//
+//  ItemActionController.h
+//  Download
+//
+//  Created by  on 11-11-11.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemActionController : UIViewController
+
+@end
