132bbe0b5fe740ee8e7898f355eaa506a486f849
Benson Peng <penglingzhe@gmail.com>
2011-11-13 22:42:28 +0800

    add file player

---
+++ DownloadCommon/Controller/Action/CommonFileActionController.h
@@ -0,0 +1,13 @@
+//
+//  CommonFileActionController.h
+//  Download
+//
+//  Created by  on 11-11-13.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonFileActionController : UIViewController
+
+@end
---
+++ DownloadCommon/Controller/Action/PlayAudioVideoController.h
@@ -0,0 +1,13 @@
+//
+//  PlayAudioVideoController.h
+//  Download
+//
+//  Created by  on 11-11-13.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayAudioVideoController : UIViewController
+
+@end
