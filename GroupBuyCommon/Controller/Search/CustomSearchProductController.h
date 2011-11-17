a08fab0c2a7b47138cdf11d0d9adf1dbe0906bc5
Benson Peng <penglingzhe@gmail.com>
2011-11-17 09:33:46 +0800

    update for quan app submission fix bug of search notification

---
+++ GroupBuyCommon/Controller/Search/CustomSearchProductController.h
@@ -0,0 +1,40 @@
+//
+//  SearchProductController.h
+//  groupbuy
+//
+//  Created by qqn_pipi on 11-8-6.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PPViewController.h"
+
+//#import "ASREngine.h"
+
+@interface CustomSearchProductController : PPViewController <UISearchBarDelegate, UIAlertViewDelegate> {
+    UIButton *latestSearchButton1;
+	UIButton *latestSearchButton2;
+	UIButton *latestSearchButton3;
+    IBOutlet UITextField *searchTextField;
+    IBOutlet UIButton *searchButton;
+    IBOutlet UISearchBar *keywordSearchBar;
+    IBOutlet UIImageView *searchTextFieldBackground;
+    IBOutlet UIImageView *searchBackgroundView;
+
+//    ASREngine   *asrEngine;
+}
+@property (retain, nonatomic) IBOutlet UIButton *testButton;
+@property (retain, nonatomic) IBOutlet UIImageView *searchTextFieldBackgroundView;
+@property (retain, nonatomic) IBOutlet UIButton *searchButton;
+@property (retain, nonatomic) IBOutlet UITextField *searchTextField;
+@property (retain, nonatomic) IBOutlet UIImageView *searchBackgroundView;
+
+@property (nonatomic, retain) IBOutlet UIButton *latestSearchButton1;
+@property (nonatomic, retain) IBOutlet UIButton *latestSearchButton2;
+@property (nonatomic, retain) IBOutlet UIButton *latestSearchButton3;
+
+
+- (IBAction) doSearch:(id)sender;
+- (IBAction) speechSearch:(id)sender;
+- (IBAction) clickSearchButton:(id)sender;
+@end
