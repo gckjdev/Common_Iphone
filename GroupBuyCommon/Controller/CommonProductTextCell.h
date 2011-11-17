2318a7c6d00076f8b919c1dc33b976f5ff5ca099
Benson Peng <penglingzhe@gmail.com>
2011-11-17 23:12:15 +0800

    add common product text cell protocol add taobao product text cell

---
+++ GroupBuyCommon/Controller/CommonProductTextCell.h
@@ -0,0 +1,13 @@
+//
+//  CommonProductTextCell.h
+//  groupbuy
+//
+//  Created by  on 11-11-17.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CommonProductTextCell <NSObject>
+
+@end
