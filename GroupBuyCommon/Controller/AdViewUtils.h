1b1742c3e4dffbced79cc7fb76dd0263f037769b
Benson Peng <penglingzhe@gmail.com>
2011-11-20 22:26:44 +0800

    add ad view support

---
+++ GroupBuyCommon/Controller/AdViewUtils.h
@@ -0,0 +1,13 @@
+//
+//  AdViewUtils.h
+//  groupbuy
+//
+//  Created by  on 11-11-20.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdViewUtils : NSObject
+
+@end
---
+++ GroupBuyCommon/Controller/AdViewUtils.m
@@ -0,0 +1,13 @@
+//
+//  AdViewUtils.m
+//  groupbuy
+//
+//  Created by  on 11-11-20.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AdViewUtils.h"
+
+@implementation AdViewUtils
+
+@end
