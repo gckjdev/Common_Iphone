5b29e8594f5463eac4b53f8ff0c4437e6996cd6f
haodong <qiuhaodong2006@126.com>
2011-11-22 10:05:07 +0800

    add show alert text

---
+++ Components/ShowAlertText/ShowAlertTextViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowAlertTextViewController.h
+//  FootballScore
+//
+//  Created by haodong qiu on 11-11-22.
+//  Copyright (c) 2011年 orange. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowAlertTextViewController : UIViewController
+
+@end
