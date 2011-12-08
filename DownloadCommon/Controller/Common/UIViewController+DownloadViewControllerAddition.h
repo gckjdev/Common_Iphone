3fae1aeecbbbe0df467c6c1b50cc54633305bdbf
Benson Peng <penglingzhe@gmail.com>
2011-12-08 16:50:30 +0800

    add download common addition

---
+++ DownloadCommon/Controller/Common/UIViewController+DownloadViewControllerAddition.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+DownloadViewControllerAddition.h
+//  Download
+//
+//  Created by  on 11-12-8.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (DownloadViewControllerAddition)
+
+@end
---
+++ DownloadCommon/Controller/Common/UIViewController+DownloadViewControllerAddition.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+DownloadViewControllerAddition.m
+//  Download
+//
+//  Created by  on 11-12-8.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIViewController+DownloadViewControllerAddition.h"
+
+@implementation UIViewController (DownloadViewControllerAddition)
+
+@end
