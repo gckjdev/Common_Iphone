6c8a9419ecdb5aa37c8f734b9e09ff8c92874bbf
Benson Peng <penglingzhe@gmail.com>
2011-12-09 14:22:05 +0800

    add audio player support

---
+++ DownloadCommon/Controller/Action/PlayAudioController.h
@@ -0,0 +1,13 @@
+//
+//  PlayAudioController.h
+//  Download
+//
+//  Created by  on 11-12-9.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlayAudioController : NSObject
+
+@end
---
+++ DownloadCommon/Controller/Action/PlayAudioController.m
@@ -0,0 +1,13 @@
+//
+//  PlayAudioController.m
+//  Download
+//
+//  Created by  on 11-12-9.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlayAudioController.h"
+
+@implementation PlayAudioController
+
+@end
