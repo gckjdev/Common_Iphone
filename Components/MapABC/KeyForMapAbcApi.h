96c69b09f9c4edc8c04c4fb486025f5696015e0d
GamyHuang <hguangm2009@gmail.com>
2011-12-12 21:25:44 +0800

    add a file to store the map abc api key.

--- Components/MapABC/KeyForMapAbcApi.h
+++ Components/MapABC/KeyForMapAbcApi.h
@@ -1,13 +1,8 @@
 //
 //  KeyForApi.h
-//  Download
 //
 //  Created by  on 11-12-12.
 //  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@protocol KeyForApi <NSObject>
-
-@end
+#define KEY @"c2b0f58a6f09cafd1503c06ef08ac7aeb7ddb91a2ca63a857416fd536d11b68c2256257c76659a83"
